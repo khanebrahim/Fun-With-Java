fbbaf380f1ca7abc9bb51dc95bde3953f06f8f37
BengalDroid <bengaldroid@gmail.com>
2021-11-11 13:28:57 +0600

    total number

---
+++ src/com/company1/variable1.java
@@ -0,0 +1,12 @@
+package com.company1;
+
+public class variable1 {
+    public static void main (String[]args){
+        System.out.println("Ebrahim khan");
+        int firstNumber = 18;
+        int secondNumber =29;
+        int thirdNumber =34;
+        int totalNumber = firstNumber + secondNumber +thirdNumber;
+        System.out.println(totalNumber);
+    }
+}
