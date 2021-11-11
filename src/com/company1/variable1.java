0ecba06cbc04c761d587ccc1b67c2e8151d1b625
BengalDroid <bengaldroid@gmail.com>
2021-11-11 13:28:57 +0600

    total number

---
+++ src/com/company1/variable1.java
@@ -0,0 +1,18 @@
+package com.company1;
+
+public class variable1 {
+    public static void main (String[]args){
+        System.out.println("Ebrahim khan");
+        int firstNumber = 18;
+        int secondNumber =29;
+        int thirdNumber =34;
+        int totalNumber = firstNumber + secondNumber +thirdNumber;
+        System.out.println(totalNumber);
+        int minIntvalue = Integer.MIN_VALUE;
+        int maxIntvalue =Integer.MAX_VALUE;
+        System.out.println(minIntvalue);
+        System.out.println(maxIntvalue);
+        System.out.println(Integer.SIZE);
+
+    }
+}
