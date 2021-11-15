2e82b69e5caca369d6254c25e10585ca4f3fe0cb
BengalDroid <bengaldroid@gmail.com>
2021-11-15 19:25:48 +0600

    total number and integer size

---
+++ src/com/company1/int_to_binary.java
@@ -0,0 +1,8 @@
+package com.company1;
+
+public class int_to_binary {
+    public static void main(String[] args) {
+        int x =5;
+        System.out.println(Integer.toBinaryString(x));
+    }
+}
